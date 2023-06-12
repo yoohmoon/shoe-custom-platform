2d36bbfc4f37f8a8cea44644ddf8e7e913b06fc8
yoohmoon <ansekfzhd5455@naver.com>
2023-06-12 10:19:39 +0900

    first commit

---
+++ src/lib/components/ExCanvas.svelte
@@ -0,0 +1,52 @@
+<!-- <script lang="ts">
+	import { browser } from '$app/environment'; 
+	import * as THREE from "three"
+
+	if(browser) {
+		let camera : THREE.PerspectiveCamera;
+		let scene : THREE.Scene;
+		let renderer : THREE.WebGLRenderer;
+		let cube : THREE.Mesh<THREE.BoxGeometry, THREE.MeshBasicMaterial>;
+
+		const init = () => {
+			scene = new THREE.Scene();
+			camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+			renderer = new THREE.WebGLRenderer();
+			renderer.setSize( window.innerWidth, window.innerHeight );
+			document.body.appendChild( renderer.domElement );
+
+			const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+			const material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+			cube = new THREE.Mesh( geometry, material );
+			scene.add( cube );
+
+			camera.position.z = 5;
+		}
+
+		const render = () => {
+			renderer.clear()
+			renderer.render(scene, camera);
+		}
+
+		const animate = () => {
+			requestAnimationFrame(animate)
+
+			cube.rotation.x += 0.005
+			cube.rotation.y += 0.005
+
+			render()
+		}
+
+		init()
+		animate()
+	}
+</script>
+
+<svelte:head>
+	<title>SvelteKit + ThreeJS</title>
+</svelte:head>
+
+<section>
+	Hello World.
+</section> -->
---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+  //   import * as THREE from "three";
+  // import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+  // let scene = new THREE.Scene();
+  // let renderer = new THREE.WebGLRenderer({
+  //   canvas:document.querySelector('#canvas')
+  // })
+
+  // let loader = new GLTFLoader()
+  // loader.load('/rocket.gltf')
+  // renderer.render(scene)
+
+  // import ThreeCanvas from "$lib/components/ThreeCanvas.svelte";
+  import NewCanvas from "../lib/components/NewCanvas.svelte";
+</script>
+
+<NewCanvas/>
+
+<!-- <ThreeCanvas/> -->
+<!-- <canvas id="canvas" width="300" height="300"></canvas> -->
