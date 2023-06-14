d96ad335a61fadcfb679a9a66b6bcc47023576c8
yoohmoon <ansekfzhd5455@naver.com>
2023-06-14 17:35:16 +0900

    Modify : page 주석 제거

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,50 +3,6 @@
   import VanilaThree from "$lib/components/VanilaThree.svelte";
   import Shoes from "$lib/components/Shoes.svelte";
 
-//  import {writable} from 'svelte/store'
-
-// import {theme} from '../store'
-
-//  const theme = writable('light');
-
-  // let bgMode = writable({   dark: 'black',
-  //   white: 'white'})
-
-  // // const bgMode = {
-  // //   dark: 'black',
-  // //   white: 'white'
-  // // };
-
-  // let color = 'white';
-  // function changeColor(color){
-  //   if(color==='white'){
-  //     color==='black'
-  //   } else if( color==='black'){
-  //     color==='white'
-  //   }
-
-  //   console.log(color)
-  // }
-  // function toggleTheme(){
-  //   // $theme = $theme==='light'?'dark':'light'
-  //   // theme.set($theme)
-  //   $theme.update(currentTheme=> (currentTheme==="light"? "dark":"light"))
-  // }
-
-  // let items = ['dark','light'];
-  // let activeItem = 'light';
-  // const themeChange = (e) =>{
-  //   activeItme = e.detail;
-  // }
 </script>
 
-<!-- <NewCanvas/> -->
-
-<!-- <div class="relative">
-  <button class="absolute" on:click={toggleTheme}>{ $theme === 'light'? "Day" : "Night" }</button>
-  <VanilaThree {theme} />
-</div> -->
-
-
-<!-- <VanilaThree/> -->
 <Shoes/>
