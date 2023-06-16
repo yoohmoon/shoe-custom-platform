a768bf19f9c7cea7ab4ac54e1a6e606c0b9e4ad3
yoohmoon <ansekfzhd5455@naver.com>
2023-06-16 15:50:57 +0900

    on progress

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script>
   import NewCanvas from "$lib/components/NewCanvas.svelte";
   import VanilaThree from "$lib/components/VanilaThree.svelte";
-  import Shoes from "$lib/components/Shoes.svelte";
+  // import Shoes from "$lib/components/Shoes.svelte";
+  import ShoesMaterial from "../lib/components/ShoesMaterial.svelte";
 
 </script>
 
-<Shoes/>
+<!-- <Shoes/> -->
+<ShoesMaterial/>
